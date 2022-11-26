c041af3e4ee55ba84680c9490e5d6e58aafdd681
rorosi <56014938+rorosi@users.noreply.github.com>
2022-11-26 17:39:09 +0900

    Create ioctl.c

---
+++ ioctl/ioctl.c
@@ -0,0 +1,40 @@
+#include <stdio.h>  // printf()
+#include <unistd.h> // close(), read(), write()
+#include <sys/fcntl.h>  // open()
+#include <sys/ioctl.h>  // ioctl()
+
+#define CALL_PRINT 1
+
+int   main(void) {
+  int fd;
+  char buf[1000];
+  int read_ret, write_ret;
+
+
+  fd = open("/dev/driver", O_RDWR);
+  if (fd < 0)
+  {
+    printf("failed opening device\n");
+    return 0;
+  }
+  else
+  {
+    printf("succeess opening device\n");
+  }
+
+  write_ret = write(fd, "hello", 5);
+  read_ret = read(fd, buf, 5);
+  printf("fd = %d, ret write = %d, ret read = %d\n", fd, write_ret, read_ret);
+  printf("content = %s\n", buf);
+
+  if(ioctl(fd, CALL_PRINT, NULL))
+  {
+    printf("failed ioctl\n");
+  }
+  else
+  {
+    printf("success ioctl\n");
+  }
+
+  close(fd);
+}
